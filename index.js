28ae564ffd1ccc638e915c072dea412bd454aa09
Lando Rudolph <lando.rudolph@pi-data.de>
2020-11-28 11:43:07 +0100

    Change index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path")
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use('/public', express.static(path.join(_dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get("/", (reg,res ) => {
